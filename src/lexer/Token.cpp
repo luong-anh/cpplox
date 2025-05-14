ce6e52b38d5b8655f48767da72f11e2909b7d7a9
Anh Luong <luonganh0128@gmail.com>
2025-05-14 01:55:40 -0700

    fix formatting

--- src/lexer/Token.cpp
+++ src/lexer/Token.cpp
@@ -1,6 +1,6 @@
 #include "Token.h"
 
 Token::Token(TokenType type, std::string&& lexeme, std::string&& literal, int line)
-    : type(type), lexeme(std::move(lexeme)), literal(std::move(literal)), line(line) {};
+    : type(type), lexeme(std::move(lexeme)), literal(std::move(literal)), line(line){};
 
 std::string Token::toString() { return TokenTypeNames.at(type) + " " + lexeme + " " + literal; }
